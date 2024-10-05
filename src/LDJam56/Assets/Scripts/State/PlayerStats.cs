a591a4988fe8998c05ae4cac5589991f5915e443
Noah Reinagel <noah.r9@gmail.com>
2024-10-05 13:16:39 -0700

    made teleporting work correctly and damage the player when it happens

--- src/LDJam56/Assets/Scripts/State/PlayerStats.cs
+++ src/LDJam56/Assets/Scripts/State/PlayerStats.cs
@@ -3,5 +3,7 @@
 [Serializable]
 public class PlayerStats
 {
-    public float Speed = 1; 
+    public float Speed = 1;
+    public float MaxLife = 5;
+    public float CurrentLife = 5;
 }
--- src/LDJam56/Assets/Scripts/World/OffMap.cs
+++ src/LDJam56/Assets/Scripts/World/OffMap.cs
@@ -1,18 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class OffMap : MonoBehaviour
 {
     [SerializeField] private float secondsPerPlayerPositionSnapshot;
     [SerializeField] private float snapshotsToSave;
-    
+
     private GameObject _player;
     private float _t;
-    
+    private Queue<Vector3> _previousPlayerLocations;
+
     private void Start()
     {
         _player = GameObject.FindWithTag("Player");
+        _t = secondsPerPlayerPositionSnapshot;
+        _previousPlayerLocations = new Queue<Vector3>();
     }
-    
-    
+
+    private void Update()
+    {
+        _t -= Time.deltaTime;
+        if (_t <= 0)
+        {
+            _previousPlayerLocations.Enqueue(_player.transform.position);
+            _t += secondsPerPlayerPositionSnapshot;
+            if (_previousPlayerLocations.Count > snapshotsToSave)
+                _previousPlayerLocations.Dequeue();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CurrentGameState.UpdateState(s => s.PlayerStats.CurrentLife = Math.Max(0, s.PlayerStats.CurrentLife - 1));
+        Message.Publish(new PlayerDamaged());
+        var location = _previousPlayerLocations.Dequeue();
+        _previousPlayerLocations = new Queue<Vector3>();
+        _previousPlayerLocations.Enqueue(location);
+        Message.Publish(new TeleportPlayer { NewPosition = location });
+    }
 }
