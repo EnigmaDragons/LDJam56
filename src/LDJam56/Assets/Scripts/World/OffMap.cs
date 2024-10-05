d16a9d43b56e0fdf592f67a9f13e17970410eef6
Noah Reinagel <noah.r9@gmail.com>
2024-10-05 01:26:01 -0700

    added cute robot enemies

---
+++ src/LDJam56/Assets/Scripts/World/OffMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class OffMap : MonoBehaviour
+{
+    [SerializeField] private float secondsPerPlayerPositionSnapshot;
+    [SerializeField] private float snapshotsToSave;
+    
+    private GameObject _player;
+    private float _t;
+    
+    private void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+    }
+    
+    
+}
