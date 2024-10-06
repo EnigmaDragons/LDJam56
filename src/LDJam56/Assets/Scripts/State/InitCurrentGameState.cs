0709b4e922c634c6193c5af8261f25e156ef021e
SilasReinagel <silas.reinagel@gmail.com>
2024-10-06 00:34:53 -0500

    Integrate: Credits Music Trigger. Closes #122

--- src/LDJam56/Assets/Scripts/State/InitCurrentGameState.cs
+++ src/LDJam56/Assets/Scripts/State/InitCurrentGameState.cs
@@ -4,5 +4,15 @@ public sealed class InitCurrentGameState : MonoBehaviour
 {
     [SerializeField] private GameplayRules rules;
     
-    void Awake() => CurrentGameState.Init(new GameState { PlayerStats = new PlayerStats { MaxLife = rules.PlayerHealth, CurrentLife = rules.PlayerHealth }});
+    void Awake()
+    {
+        if (rules == null)
+        {
+            Log.Error("GameplayRules is null", this);
+        }
+        else
+        {
+            CurrentGameState.Init(new GameState { PlayerStats = new PlayerStats { MaxLife = rules.PlayerHealth, CurrentLife = rules.PlayerHealth } });
+        }
+    }
 }
