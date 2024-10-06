c775dd02798b69b249e8bc7f430fbd1dac2a1a05
Noah Reinagel <noah.r9@gmail.com>
2024-10-06 01:45:07 -0700

    added sprint particles

--- src/LDJam56/Assets/Scripts/Abilities/Speed.cs
+++ src/LDJam56/Assets/Scripts/Abilities/Speed.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class Speed : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem particleSystem;
+    
     private Action _onComplete;
     private float _duration;
-
+    private float _timeTilDeath;
+    
     public void Init(AbilityData data, AbilityType type, AbilityData[] nextAbilities)
     {
         CurrentGameState.UpdateState(s => s.PlayerStats.Speed += data.Amount);
@@ -15,11 +18,21 @@ public class Speed : MonoBehaviour
 
     private void Update()
     {
-        _duration -= Time.deltaTime;
-        if (_duration <= 0)
+        if (_timeTilDeath > 0)
         {
-            _onComplete();
-            Destroy(this);
+            _timeTilDeath -= Time.deltaTime;
+            if (_timeTilDeath <= 0)
+                Destroy(gameObject);
+        }
+        else
+        {
+            _duration -= Time.deltaTime;
+            if (_duration <= 0)
+            {
+                _onComplete();
+                particleSystem.Stop();
+                _timeTilDeath = 1;
+            }   
         }
     }
 }
