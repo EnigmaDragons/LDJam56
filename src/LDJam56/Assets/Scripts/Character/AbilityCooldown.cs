f4f051d0a6a4459b3992a35cc5dc67c9875d0fae
Noah Reinagel <noah.r9@gmail.com>
2024-10-06 03:55:39 -0700

    added cooddown uis

---
+++ src/LDJam56/Assets/Scripts/Character/AbilityCooldown.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using RengeGames.HealthBars;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityCooldown : MonoBehaviour
+{
+    [SerializeField] private RadialSegmentedHealthBar cooldownRadial;
+    [SerializeField] private AbilityType type;
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private GameplayRules rules;
+    [SerializeField] private Image image;
+
+    private bool _onCooldown = false;
+
+    private void Start()
+    {
+        cooldownRadial.SetPercent(0);
+        _onCooldown = false;
+        text.text = "";
+        image.enabled = false;
+    }
+
+    private void Update()
+    {
+        var cooldown = GetCooldown();
+        var baseCooldown = GetBaseCooldown();
+        if (_onCooldown && cooldown <= 0)
+        {
+            _onCooldown = false;
+            text.text = "";
+            image.enabled = false;
+            cooldownRadial.SetPercent(0);
+        }
+        else if (!_onCooldown && cooldown > 0)
+        {
+            _onCooldown = true;
+            cooldownRadial.SetPercent(1);
+            image.enabled = true;
+        }
+        
+        if (_onCooldown)
+        {
+            text.text = Math.Ceiling(cooldown).ToString();
+            cooldownRadial.SetPercent(cooldown / baseCooldown);
+        }
+    }
+
+    private float GetCooldown()
+    {
+        if (type == AbilityType.Attack)
+            return CurrentGameState.ReadonlyGameState.Attack.CooldownRemaining;
+        if (type == AbilityType.Special)
+            return CurrentGameState.ReadonlyGameState.Special.CooldownRemaining;
+        if (type == AbilityType.Mobility)
+            return CurrentGameState.ReadonlyGameState.Mobility.CooldownRemaining;
+        if (type == AbilityType.Defense)
+            return CurrentGameState.ReadonlyGameState.Defense.CooldownRemaining;
+        return 0;
+    }
+
+    private float GetBaseCooldown()
+    {
+        if (type == AbilityType.Attack)
+            return CurrentGameState.ReadonlyGameState.Attack.Cooldown(rules, CurrentGameState.ReadonlyGameState.PlayerStats);
+        if (type == AbilityType.Special)
+            return CurrentGameState.ReadonlyGameState.Special.Cooldown(rules, CurrentGameState.ReadonlyGameState.PlayerStats);
+        if (type == AbilityType.Mobility)
+            return CurrentGameState.ReadonlyGameState.Mobility.Cooldown(rules, CurrentGameState.ReadonlyGameState.PlayerStats);
+        if (type == AbilityType.Defense)
+            return CurrentGameState.ReadonlyGameState.Defense.Cooldown(rules, CurrentGameState.ReadonlyGameState.PlayerStats);
+        return 0;
+    }
+}
--- src/LDJam56/Assets/Scripts/GameRules/AbilityData.cs
+++ src/LDJam56/Assets/Scripts/GameRules/AbilityData.cs
@@ -4,8 +4,8 @@
 public class AbilityData : ScriptableObject
 {
     public AbilityComponentType Type;
+    public AbilityType Primary;
     public string DisplayName;
-    public string Description;
     public float Duration;
     public float Amount;
     public float Speed;
