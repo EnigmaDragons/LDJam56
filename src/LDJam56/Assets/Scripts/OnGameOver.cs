2919c5946190806d9130f14539ca7d91f36ecd38
SilasReinagel <silas.reinagel@gmail.com>
2024-10-07 01:58:43 -0500

    Music: Game Code Hooks. Closes #72

---
+++ src/LDJam56/Assets/Scripts/OnGameOver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnGameOver : OnMessage<GameOver>
+{
+    [SerializeField] private UnityEvent e;
+    
+    protected override void Execute(GameOver msg)
+    {
+        e.Invoke();
+    }
+}
